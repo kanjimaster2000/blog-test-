ea122fb5d5d09f4017a4f1242ea939c4448efe7f
kanjimaster2000 <kanjimaster2000@users.noreply.github.com>
2017-06-19 17:05:10 +0000

    Fixing Migration files

--- db/migrate/20170615140552_create_articles.rb
+++ db/migrate/20170615140552_create_articles.rb
@@ -3,5 +3,9 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
     end
+    add_column :articles, :description, :text
+    add_column :articles, :created_at, :date_time
+    add_column :articles, :updated_at, :date_time
+    
   end
 end
